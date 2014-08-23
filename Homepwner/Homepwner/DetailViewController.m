523e98eea0162d7e4f02c7a34d023769de9ded19
looneylu <lcarbuccia@gmail.com>
2014-08-23 19:50:21 -0400

    DetailViewController
    initial commit and storyboard setup

---
+++ Homepwner/Homepwner/DetailViewController.m
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.m
+//  Homepwner
+//
+//  Created by Luis Carbuccia on 8/23/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@implementation DetailViewController
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
