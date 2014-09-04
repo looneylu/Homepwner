f2b5b4b38f9fa8283e6518f955cf528191dc4eda
looneylu <lcarbuccia@gmail.com>
2014-09-04 15:48:22 -0400

    Core Data
    initial commit

---
+++ Homepwner/Homepwner/ImageTransformer.h
@@ -0,0 +1,13 @@
+//
+//  ImageTransformer.h
+//  Homepwner
+//
+//  Created by Luis Carbuccia on 9/4/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageTransformer : NSValueTransformer
+
+@end
---
+++ Homepwner/Homepwner/ImageTransformer.m
@@ -0,0 +1,13 @@
+//
+//  ImageTransformer.m
+//  Homepwner
+//
+//  Created by Luis Carbuccia on 9/4/14.
+//  Copyright (c) 2014 Luis Carbuccia. All rights reserved.
+//
+
+#import "ImageTransformer.h"
+
+@implementation ImageTransformer
+
+@end
